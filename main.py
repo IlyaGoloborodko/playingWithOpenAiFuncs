53de39606f0f2f9dd2c9a8d1e790cd669e7fadd1
Ilya <sok203333@gmail.com>
2024-09-29 21:03:51 +0300

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Write a haiku about recursion in programming."
+        }
+    ]
+)
+
+print(completion.choices[0].message.content)
